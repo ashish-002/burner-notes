a742f621e48bb43fe0a7796b2003b83897f091c3
CyberSphinx <44707624+ashish-002@users.noreply.github.com>
2025-05-26 17:49:20 +0530

    Update1 sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,33 @@
-const CACHE_NAME = 'burner-link-v1';
+// sw.js
+const CACHE_NAME = 'burner-v5';
 const LOCAL_ASSETS = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './sw.js'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(LOCAL_ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(LOCAL_ASSETS))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
